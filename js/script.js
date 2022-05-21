5c2523da1e49ddb578fff2fd3f207bc9829fe6a5
Yurii <paraska108@gmail.com>
2022-05-21 11:29:22 +0300

    Set Taimer

--- js/script.js
+++ js/script.js
@@ -1,39 +1,83 @@
-window.addEventListener('DOMContentLoaded', function() {
-        // Tabs
-            let tabs = document.querySelectorAll('.tabheader__item'),
-                    tabsContent = document.querySelectorAll('.tabcontent'),
-                    tabsParent = document.querySelector('.tabheader__items');
-    
-            function hideTabContent() {
-            
-            tabsContent.forEach(item => {
-                item.classList.add('hide');
-                item.classList.remove('show', 'fade');
+window.addEventListener('DOMContentLoaded', function () {
+
+    // Tabs
+    let tabs = document.querySelectorAll('.tabheader__item'),
+        tabsContent = document.querySelectorAll('.tabcontent'),
+        tabsParent = document.querySelector('.tabheader__items');
+
+    function hideTabContent() {
+
+        tabsContent.forEach(item => {
+            item.classList.add('hide');
+            item.classList.remove('show', 'fade');
+        });
+
+        tabs.forEach(item => {
+            item.classList.remove('tabheader__item_active');
+        });
+    }
+
+    function showTabContent(i = 0) {
+        tabsContent[i].classList.add('show', 'fade');
+        tabsContent[i].classList.remove('hide');
+        tabs[i].classList.add('tabheader__item_active');
+    }
+
+    hideTabContent();
+    showTabContent();
+
+    tabsParent.addEventListener('click', function (event) {
+        const target = event.target;
+        if (target && target.classList.contains('tabheader__item')) {
+            tabs.forEach((item, i) => {
+                if (target == item) {
+                    hideTabContent();
+                    showTabContent(i);
+                }
             });
-    
-            tabs.forEach(item => {
-                item.classList.remove('tabheader__item_active');
-            });
-            }
-    
-            function showTabContent(i = 0) {
-            tabsContent[i].classList.add('show', 'fade');
-            tabsContent[i].classList.remove('hide');
-            tabs[i].classList.add('tabheader__item_active');
         }
-        
-        hideTabContent();
-        showTabContent();
-    
-            tabsParent.addEventListener('click', function(event) {
-                    const target = event.target;
-                    if(target && target.classList.contains('tabheader__item')) {
-                tabs.forEach((item, i) => {
-                    if (target == item) {
-                        hideTabContent();
-                        showTabContent(i);
-                    }
-                });
-                    }
-            });
     });
+
+    const deadline = '2022-05-31';
+
+    function getTimeRemaining(endtime) {
+        const t = Date.parse(endtime) - Date.parse(new Date()),
+            days = Math.floor(t / (1000 * 60 * 60 * 24)),
+            hours = Math.floor(t / (1000 * 60 * 60) % 24),
+            minutes = Math.floor((t / 1000 / 60) % 60),
+            seconds = Math.floor((t / 1000) % 60);
+        console.log();
+        return {
+            'total': t,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+    }
+
+    function setClock(selector, endtime) {
+        const timer = document.querySelector(selector),
+            days = timer.querySelector('#days'),
+            hours = timer.querySelector('#hours'),
+            minutes = timer.querySelector('#minutes'),
+            seconds = timer.querySelector('#seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+        updateClock();
+
+        function updateClock() {
+            const t = getTimeRemaining(endtime);
+
+            days.innerHTML = days.t;
+            hours.innerHTML = hours.t;
+            minutes.innerHTML = minutes.t;
+            seconds.innerHTML = seconds.t;
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+            }
+        }
+    }
+    console.log(setClock('.timer', deadline));
+});
